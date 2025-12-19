936792858e5e444ba538a8f6eb31155e738b6364
AnNguyenVan123 <annguyenvp2004@gmail.com>
2025-12-19 10:57:36 +0700

    a

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.get("/", async (req, res) => {
     res.status(200).send("Xin chào bạn");
 });
-
+//1
 // Start server
 app.listen(3000, () => {
     console.log("Server running on http://localhost:3000");
